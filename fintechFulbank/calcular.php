144399828936b165755967250d8801ced66a563e
Bruna <lozinskibruna@gmail.com>
2021-08-13 10:52:29 -0300

    atualizações

--- fintechFulbank/calcular.php
+++ fintechFulbank/calcular.php
@@ -1,20 +1,48 @@
-<link rel="stylesheet" href="style.css">
-
 <?php
 
 if (isset ($_POST["nome"]) && isset($_POST["salarioAtual"])
-    && isset($_POST["cargo"]))
+    && isset($_POST["genero"]) && isset($_POST["cargo"]))
 {
     $nome = $_POST["nome"];
     $salarioAtual = $_POST["salarioAtual"];
+    $genero = $_POST["genero"];
     $cargo = $_POST["cargo"];
 
-    $novoSalario = $_POST["novoSalario"];
-    
+    $salarioNovo = 0;
+
 } else {
-    echo "Campos não preenchidos";
+    die ("<main>Campos não preenchidos</main>");
 } 
 
-if {
+// if ($salarioAtual > 5000) {
+//     $salarioNovo = $salarioAtual * 1.1;
+// } else {
+//     $salarioNovo = $salarioAtual * 1.2;
+// }
 
-}
+$salarioNovo = $salarioAtual > 5000 ? $salarioAtual * 1.1 : $salarioAtual * 1.2;
+
+?>
+
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>Calcular salario</title>
+</head>
+<body>
+    <div class="caixa">
+    <main>
+        <p>
+            <?= $genero === "M" ? "O" : ($genero === "F" ? "A" : "") ?>
+            <?= $nome ?> passará a receber R$
+            <?= number_format($salarioNovo, 2, ",", ".") ?>
+            no cargo de <?= $cargo ?>
+        </p>
+    </main>
+    </div>
+</body>
+</html>
